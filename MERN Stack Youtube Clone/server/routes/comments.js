4cd5d6ff9c8b8c9437ec988de9aefc8607139ff6
anaydhaya <anaydhaya@hotmail.co.uk>
2022-08-13 20:02:06 +0100

    Comments API

--- MERN Stack Youtube Clone/server/routes/comments.js
+++ MERN Stack Youtube Clone/server/routes/comments.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { } from "../controllers/comment.js";
+import { addComment, deleteomment, getComments } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteomment);
+router.get("/:videoId", getComments);
 
 export default router;
--- MERN Stack Youtube Clone/server/routes/videos.js
+++ MERN Stack Youtube Clone/server/routes/videos.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addVideo, addView, deleteVideo, getVideo, random, sub, trend, updateVideo } from "../controllers/video.js";
+import { addVideo, addView, deleteVideo, getVideo, random, search, sub, getByTags, trend, updateVideo } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
@@ -27,5 +27,11 @@ router.get("/random", random);
 
 // Subscribed videos
 router.get("/sub", verifyToken, sub);
+
+// Get by tags
+router.get("/tags", getByTags);
+
+// Get by title
+router.get("/search", search);
 
 export default router;
