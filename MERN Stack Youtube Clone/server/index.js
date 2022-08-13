052837f466f20950ac4454d2e14b412b745459f4
anaydhaya <anaydhaya@hotmail.co.uk>
2022-08-13 15:32:02 +0100

    First commit
    - Project skeleton
    - Beginning routing and some testing using insomnia

---
+++ MERN Stack Youtube Clone/server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/comments.js";
+import commentRoutes from "./routes/videos.js";
+
+const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose
+        .connect(process.env.MONGO)
+        .then(() => {
+            console.log("Connected to DB!");
+        }).catch(err => {
+            throw err;
+        });
+};
+
+app.use(express.json())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/videos", videoRoutes)
+app.use("/api/comments", commentRoutes)
+
+app.listen(8000, () => {
+    connect();
+    console.log("Connected to Server!");
+})
