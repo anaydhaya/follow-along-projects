f18c989218dd135eb9d0fde8982f7a0fb2b865c2
anaydhaya <anaydhaya@hotmail.co.uk>
2022-08-13 16:20:37 +0100

    Sign in and Sign up
    Finished the Sign in and Sign up functionality
    Added generic error handling for code, message combo

--- MERN Stack Youtube Clone/server/controllers/auth.js
+++ MERN Stack Youtube Clone/server/controllers/auth.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from './../error.js';
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {
+//
+// Create a new user item in the DB
+//
+export const signup = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
@@ -12,7 +17,31 @@ export const signup = async (req, res) => {
         res.status(200).send("User has been created!");
     }
     catch (err) {
-        // todo - handle error
-        res.status(201).send("Error: " + err);
+        next(err)
+    }
+}
+
+//
+// Request the user from the DB, if user passes checks then
+// attach access token cookie to the response. Otherwise Err
+//
+export const signin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ name: req.body.name });
+        if (!user) return next(createError(404, "User not found"));
+
+        const isCorrect = await bcrypt.compare(req.body.password, user.password)
+
+        if (!isCorrect) return next(createError(400, "Wrong credentials"));
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT)
+        const { password, ...others } = user._doc;
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(others);
+    }
+    catch (err) {
+        next(err)
     }
 }
--- MERN Stack Youtube Clone/server/index.js
+++ MERN Stack Youtube Clone/server/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/comments.js";
 import commentRoutes from "./routes/videos.js";
+import cookieParser from "cookie-parser";
 
 const app = express()
 dotenv.config()
@@ -19,11 +20,23 @@ const connect = () => {
         });
 };
 
+app.use(cookieParser());
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/videos", videoRoutes)
 app.use("/api/comments", commentRoutes)
+
+// How we handle errors in express - middleware
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        status: status,
+        message: message
+    })
+})
 
 app.listen(8000, () => {
     connect();
